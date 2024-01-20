6830f20ede7d139bd8c2000ab1b0cdbeee65e488
fernandes-draw <fernandes_draw@hotmail.com>
2024-01-20 17:06:28 -0300

    Functionality to edit products

--- src/main/java/com/curso/ecommerce/controller/ProductController.java
+++ src/main/java/com/curso/ecommerce/controller/ProductController.java
@@ -1,11 +1,14 @@
 package com.curso.ecommerce.controller;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -41,6 +44,24 @@ public class ProductController {
 		product.setUsuario(u);
 
 		productService.save(product);
+		return "redirect:/products";
+	}
+
+	@GetMapping("/edit/{id}")
+	public String edit(@PathVariable Integer id, Model model) {
+		Product product = new Product();
+		Optional<Product> optionalProduct = productService.get(id);
+		product = optionalProduct.get();
+
+		LOGGER.info("Product find: {}", product);
+
+		model.addAttribute("product", product);
+		return "products/edit";
+	}
+
+	@PostMapping("/update")
+	public String update(Product product) {
+		productService.update(product);
 		return "redirect:/products";
 	}
 
